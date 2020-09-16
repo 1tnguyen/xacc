d245d9e8ffc1b82f04ff568ea5868608e103c5fc
Thien Nguyen <nguyentm@ornl.gov>
2020-09-16 16:30:49 -0400

    Pushed placement benchmarking script + source files
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/examples/placement/placement_run.cpp
+++ quantum/examples/placement/placement_run.cpp
@@ -1,22 +1,42 @@
 #include "xacc.hpp"
 #include <filesystem>
 
+std::pair<size_t, size_t> countGates(std::shared_ptr<xacc::CompositeInstruction>& in_composite) 
+{
+    size_t nbSingleQubitGates = 0;
+    size_t nbTwoQubitGates = 0;
+    for (const auto& inst : in_composite->getInstructions())
+    {
+        if (inst->bits().size() == 1) {
+            ++nbSingleQubitGates;
+        }
+        if (inst->bits().size() == 2) {
+            ++nbTwoQubitGates;
+        }
+    }
+    return std::make_pair(nbSingleQubitGates, nbTwoQubitGates);
+}
+
 int main(int argc, char **argv) {
     xacc::Initialize(argc, argv);
-    const std::vector<std::string> PLACEMENT_PLUGINS { "swap-shortest-path", "triQ", "enfield" };
-    const std::vector<std::string> ENFIELD_ALLOCATORS{
-        "Q_dynprog",  "Q_grdy",
-        "Q_bmt",      "Q_simplified_bmt",
-        "Q_ibmt",     "Q_simplified_ibmt",
-        "Q_opt_bmt",  "Q_layered_bmt",
-        "Q_ibm",      "Q_wpm",
-        "Q_random",   "Q_qubiter",
-        "Q_wqubiter", "Q_jku",
-        "Q_sabre",    "Q_chw"};
+    // const std::vector<std::string> PLACEMENT_PLUGINS { "swap-shortest-path", "enfield", "triQ" };
+    const std::vector<std::string> PLACEMENT_PLUGINS { "swap-shortest-path", "enfield" };
+    auto rotationFolding = xacc::getIRTransformation("rotation-folding");
 
+    // const std::vector<std::string> ENFIELD_ALLOCATORS{
+    //     "Q_dynprog",  "Q_grdy",
+    //     "Q_bmt",      "Q_simplified_bmt",
+    //     "Q_ibmt",     "Q_simplified_ibmt",
+    //     "Q_opt_bmt",  "Q_layered_bmt",
+    //     "Q_ibm",      "Q_wpm",
+    //     "Q_random",   "Q_qubiter",
+    //     "Q_wqubiter", "Q_jku",
+    //     "Q_sabre",    "Q_chw"};
+    const std::vector<std::string> ENFIELD_ALLOCATORS{
+        "Q_ibm", "Q_wpm", "Q_sabre", "Q_chw" };
     auto compiler = xacc::getCompiler("staq");
     // May need to choose a larger chip to make sure it can accommodate these circuits.
-    auto accelerator = xacc::getAccelerator("ibm:ibmq_16_melbourne");
+    auto accelerator = xacc::getAccelerator("ibm:ibmq_manhattan");
     const std::string srcDirRath = std::string(QASM_SOURCE_DIR);
     for (const auto& entry : std::filesystem::directory_iterator(srcDirRath)) {
         const std::string sourceFile = entry.path().c_str();
@@ -36,9 +56,13 @@ int main(int argc, char **argv) {
                     // Compile:
                     auto IR = compiler->compile(qasmSrcStr);
                     auto program = IR->getComposites()[0];
+                    rotationFolding->apply(program, nullptr);
+                    auto [before_1q, before_2q] = countGates(program);
+                    std::cout << "BEFORE: " << program->nInstructions() << "(" << before_1q << ", " << before_2q << ")\n";
                     irt->apply(program, accelerator,
                                {{"allocator", allocator}});
-                    std::cout << "HOWDY: \n" << program->nInstructions() << "\n";
+                    auto [after_1q, after_2q] = countGates(program);
+                    std::cout << "AFTER: " << program->nInstructions() << "(" << after_1q << ", " << after_2q << ")\n";
                 }
             }
             else 
@@ -47,9 +71,13 @@ int main(int argc, char **argv) {
                 // Compile:
                 auto IR = compiler->compile(qasmSrcStr);
                 auto program = IR->getComposites()[0];
+                rotationFolding->apply(program, nullptr);
+                auto [before_1q, before_2q] = countGates(program);
+                std::cout << "BEFORE: " << program->nInstructions() << "(" << before_1q << ", " << before_2q << ")\n";
                 irt->apply(program, accelerator);
                 // TODO: analyze the post-placement circuit
-                std::cout << "HOWDY: \n" << program->nInstructions() << "\n";
+                auto [after_1q, after_2q] = countGates(program);
+                std::cout << "AFTER: " << program->nInstructions() << "(" << after_1q << ", " << after_2q << ")\n";
             }
         }       
     }   
