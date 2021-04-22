d685ae846173e800f845aef9dc03de4431dc9c14
Thien Nguyen <nguyentm@ornl.gov>
2021-04-22 17:25:28 -0400

    Hook execution_info to Pulse Transform
    Look for openpulse data from the accelerator's execution info to construct parameters.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/optimizers/pulse/PulseTransform.hpp
+++ quantum/plugins/optimizers/pulse/PulseTransform.hpp
@@ -4,18 +4,31 @@
 
 namespace xacc {
 namespace quantum {
-class PulseTransform : public IRTransformation 
-{
+class PulseTransform : public IRTransformation {
 public:
   PulseTransform() {}
   void apply(std::shared_ptr<CompositeInstruction> program,
-                     const std::shared_ptr<Accelerator> accelerator,
-                     const HeterogeneousMap& options = {}) override; 
-  
-  const IRTransformationType type() const override { return IRTransformationType::Optimization; } 
+             const std::shared_ptr<Accelerator> accelerator,
+             const HeterogeneousMap &options = {}) override;
+
+  const IRTransformationType type() const override {
+    return IRTransformationType::Optimization;
+  }
 
   const std::string name() const override { return "quantum-control"; }
   const std::string description() const override { return ""; }
+  // Pulse optimization info:
+  struct OptInfo {
+    // Static H
+    std::string H0;
+    // Control channels
+    std::vector<std::string> controlOps;
+    std::vector<std::string> controlChannels;
+    double dt;
+    std::string ham_json;
+  };
+
+  OptInfo parseDeviceInfo(const HeterogeneousMap &exe_data) const;
 };
 } // namespace quantum
 } // namespace xacc
